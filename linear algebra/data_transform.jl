2c05e67cad0039edf4e7502f91337236ea64e413
Jorsorokin <Jorsorokin@gmail.com>
2019-04-17 09:36:50 -0700

    finished linear algebra code

--- linear algebra/data_transform.jl
+++ linear algebra/data_transform.jl
@@ -1,25 +1,8 @@
 import LinearAlgebra
-import DelimitedFiles
 import StatsBase
-using Statistics, Printf, Plots
-
-datapath = "/Users/Jordan/Git/hlab-analysis-methods/data"
-file = "NMDA_puff1.atf"
-
-@printf("Loading the data matrix 'M'\n")
-M = DelimitedFiles.readdlm(joinpath(datapath, file));
-T = M[:,1]
-M = M[:,2:end]
-nPts, nSweeps = size(M)
-@printf("M has %i time points, and %i sweeps\n", nPts, nSweeps)
-plot(T, M[:,[1, 5, nSweeps]], label=["first", "middle", "last"], xlab="t (s)", ylab="mV")
+using Statistics, Plots
 
 ### DATA SCALING
-# Z-transform the data
-μ = mean(M)
-σ = std(M)
-M = StatsBase.zscore(M)
-
 # visualizing different transforms
 x = randn(100)
 y = randn(100)*4
@@ -33,6 +16,7 @@ y_n = y ./ StatsBase.norm(y)
 
 plot(x_z, y_z, seriestype=:scatter, xlim=[-5 5], ylim = [-5 5])
 plot!(x_n, y_n, seriestype=:scatter, xlim=[-5 5], ylim = [-5 5])
+
 
 ### MATRIX PRODUCTS
 # smooth a segment of data using (a) loops, (b) matrix multiplication
@@ -49,9 +33,48 @@ end
 A = LinearAlgebra.Tridiagonal(fill(0.25, 99), fill(0.5, 100), fill(0.25, 99))
 smooth_x2 = A * x
 
-plot(x, size=(500, 500))
-plot!(smooth_x)
-plot!(smooth_x2)
+plot(x, size=(500, 500), label = "original", lw=2)
+plot!(smooth_x, label = "smoothed with loop", lw=2)
+plot!(smooth_x2, label = "smoothed with matrix product", lw=2)
 
 
-### DATA TRANSFORMATIONS
+### DATA ROTATIONS/TRANSFORMS
+f(x) = 2x .+ 5.0
+x = Array(range(0, stop=10, length=100))
+data = hcat(x, f(x)) + randn(100,2)
+plot(view(data, :, 1), view(data, :, 2), seriestype=:scatter,
+    xlim = [0, 30], ylim = [-30, 30], label="original")
+
+# make a rotation matrix around the y-axis
+A = [1 0; 0 -1]
+data2 = data * A
+plot!(view(data2, :, 1), view(data2, :, 2), seriestype=:scatter,
+    label = "(1x, -1y)")
+
+# squash the y-vals by 50%, expand x-vals by 100%
+A = [2 0; 0 0.5]
+data2 = data * A
+plot!(view(data2, :, 1), view(data2, :, 2), seriestype=:scatter,
+    label = "(2x, 0.5y)")
+
+
+### DATA PROJECTIONS
+# now let's project the data onto subspaces
+A_x = [1 0; 0 0] # x-axis projection
+A_y = [0 0; 0 1] # y-axis projection
+A_xy = [0.3 0.5; 0.1 -0.6] # different linear combinations of x and y,
+datax = data * A_x
+datay = data * A_y
+dataxy = data * A_xy
+plot(view(data, :, 1), view(data, :, 2), seriestype=:scatter,
+    xlim = [0, 30], ylim = [-30, 30], label="original")
+plot!(view(datax, :, 1), view(datax, :, 2), seriestype=:scatter,
+    label = "(1x+0y, 0x+0y)")
+plot!(view(datay, :, 1), view(datay, :, 2), seriestype=:scatter,
+    label = "(0x+0y, 0x+1y)")
+plot!(view(dataxy, :, 1), view(dataxy, :, 2), seriestype=:scatter,
+    label = "(.3x+.1y, .5x-.6y)")
+
+# the linear matrices "A"" are performing linear functions on the columns of "data"
+f_xy(data) = hcat(0.3data[:,1] + 0.1data[:,2], 0.5data[:,1] - 0.6data[:,2])
+isapprox(dataxy, f_xy(data))
